0fb0536649f33502cb66276646c0582fd2c06a2b
ignacio chiazzo <ignaciochiazzo@ignacios-MacBook-Air.local>
2021-09-24 19:42:42 -0400

    Improve message log

--- lib/middleware/measure_logs.rb
+++ lib/middleware/measure_logs.rb
@@ -6,26 +6,20 @@ module Middleware
 
     def call(env)
       start_time = Time.now
-      request = @app.call(env)
+      status, headers, response = @app.call(env)
       end_time = Time.now
-      status, headers, response = request
-
       total_time = calculate_total_time(start_time, end_time)
-      log_status(total_time, status)
-      add_request_time_header(total_time, headers)
+      log_status(total_time, status, env)
 
       [status, headers, response]
     end
 
     private
 
-    def log_status(total_time, status)
-      # Store the status result somewhere.
-      Rails.logger.info("----- [HTTP Request] status=#{status} time=#{total_time.to_s} -----")
-    end
-
-    def add_request_time_header(total_time, headers)
-      headers["X-request-time"] = total_time
+    def log_status(total_time, status, env)
+      controller_name = env['action_controller.instance'].class.name
+      action = env['action_controller.instance'].action_name
+      Rails.logger.info("----- [HTTP Request] status=#{status} time=#{total_time.to_s} processed_by=#{controller_name}\##{action} -----")
     end
 
     def calculate_total_time(start_time, end_time)
